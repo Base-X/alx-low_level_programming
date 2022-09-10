dc9530214b33a39af592e9b6b332fe84cb2fcb2b
Ayomikun <samuel.ayomikun01@gmail.com>
2022-09-10 11:54:19 -0700

    printer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the print function
+ * main - Entry point
  *
- * return: 0
+ * return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
