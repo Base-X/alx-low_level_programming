33ed444fd36e4b55f94d118b6c4687cbf80b66c7
Ayomikun <samuel.ayomikun01@gmail.com>
2022-09-12 12:56:55 -0700

    intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Description: print out with proper grammar, but the outcome is a piece of art
- * return: Always 0 (Success)
+ *
+ * return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
